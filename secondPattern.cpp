20479482b0b400ba4ba9a822b9336563b266732d
Somnath Chakraborty <61010000+somnath352@users.noreply.github.com>
2021-11-17 19:50:55 +0530

    Update secondPattern.cpp

--- secondPattern.cpp
+++ secondPattern.cpp
@@ -10,10 +10,10 @@
 using namespace std;
 
 int main(){
-    
-    for(int i = 1;i<=5;i++) {
-        for(int j = 1;j<=5;j++) {
-            if(j >= (6 - i))
+    int n = 5;
+    for(int i = 1;i<=n;i++) {
+        for(int j = 1;j<=n;j++) {
+            if(j >= ((n+1) - i))
                 cout << "*";
             else cout << " ";
         }
