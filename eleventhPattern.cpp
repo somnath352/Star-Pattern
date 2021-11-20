24af493aae1c80f0db46b01e6b5c65a345c601b7
Somnath Chakraborty <61010000+somnath352@users.noreply.github.com>
2021-11-20 11:57:35 +0530

    update even or odd input

--- eleventhPattern.cpp
+++ eleventhPattern.cpp
@@ -2,6 +2,7 @@
 // **  
 // *** 
 // ****
+// ****
 // *** 
 // **  
 // *   
@@ -12,11 +13,17 @@ using namespace std;
 
 int main() {
 
-	int rows = 7, cols = 4, k=0;
+    int rows, cols;
+    cin >> rows;
+	int k=0;
+	int n = (rows+1) / 2;
+	if(rows%2 == 0) cols = rows/2;
+	else cols = (rows+1)/2;
 
 	for(int i = 1;i<=rows;i++) {
-
-        i<=4 ? k++ : k--;
+        if(i<=n) k++;
+        if(i>n+1) k--;
+        // i<=4 ? k++ : k--;
         
 	    for(int j = 1;j<=cols;j++) {
             if(j <= k) {
